4a2a96a4d5b20417e28f8281b7a65acacbc096a9
wasuken <wasuken1@gmail.com>
2020-12-21 20:02:28 +0900

    キャッシュ更新可否の処理追加。http/headのレスポンスのLast-Modifiedとキャ ッシュのmodを比較するように

--- common/common.go
+++ common/common.go
@@ -3,12 +3,58 @@ package common
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/Songmu/go-httpdate"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 const CACHEDIR string = "/tmp/wcgorep/"
+
+func cacheCheck(url string) {
+	urlHashPath := CACHEDIR + fmt.Sprintf("%x", sha1.Sum([]byte(url)))
+	info, err := os.Stat(urlHashPath)
+	if err != nil {
+		panic(err)
+	}
+	cacheModTime := info.ModTime()
+
+	resp, err := http.Head(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	urlLastMod, err := httpdate.Str2Time(strings.TrimSpace(resp.Header.Get("Last-Modified")), time.UTC)
+	if err != nil {
+		panic(err)
+	}
+
+	if cacheModTime.After(urlLastMod) {
+		os.Remove(urlHashPath)
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		body := string(b)
+		err = cacheSave(url, body)
+		if err != nil {
+			panic(err)
+		}
+		if cacheSave(url, body) != nil {
+			panic(err)
+		}
+	}
+}
 
 func cacheSave(url, contents string) error {
 	urlHash := fmt.Sprintf("%x", sha1.Sum([]byte(url)))
@@ -16,7 +62,7 @@ func cacheSave(url, contents string) error {
 	return ioutil.WriteFile(CACHEDIR+urlHash, []byte(contents), 0777)
 }
 
-func cacheLoad(url string) (string, bool) {
+func cacheLoad(url string) string {
 	if _, err := os.Stat(CACHEDIR); os.IsNotExist(err) {
 		if os.Mkdir(CACHEDIR, 0777) != nil {
 			panic(err)
@@ -26,35 +72,18 @@ func cacheLoad(url string) (string, bool) {
 	urlHash := fmt.Sprintf("%x", sha1.Sum([]byte(url)))
 
 	if _, err := os.Stat(CACHEDIR + urlHash); os.IsNotExist(err) {
-		return "", false
+		return ""
 	}
 	contents, err := ioutil.ReadFile(CACHEDIR + urlHash)
 	if err != nil {
 		panic(err)
 	}
-	return string(contents), true
+	return string(contents)
 }
 
 func Wget(url string) (string, error) {
-	body, ext := cacheLoad(url)
-	if !ext {
-		resp, err := http.Get(url)
-		if err != nil {
-			return "", err
-		}
-		defer resp.Body.Close()
-
-		b, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return "", err
-		}
-
-		body = string(b)
-		err = cacheSave(url, body)
-		if err != nil {
-			return "", err
-		}
-	}
+	cacheCheck(url)
+	body := cacheLoad(url)
 
 	return string(body), nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wasuken/wcgorep
 go 1.15
 
 require (
+	github.com/Songmu/go-httpdate v1.0.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.1
 )
